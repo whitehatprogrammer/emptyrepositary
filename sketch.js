ccdee20be891ea073389838ae3fca6f025a05b2a
whitehatprogrammer <aartipramodbarge@gmail.com>
2020-10-26 21:46:03 +0530

    Add platform

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform = new Ground(150,300,300,170);
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -59,4 +59,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
